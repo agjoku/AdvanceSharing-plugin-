04ad65bca38e1dc3477779df1f11c026a87b5558
agjoku <kota.319.meado@gmail.com>
2024-07-02 22:38:52 +0900

    output_csv

--- src/main/java/agjoku/avancementsharing/AdvancementSharing.java
+++ src/main/java/agjoku/avancementsharing/AdvancementSharing.java
@@ -1,5 +1,6 @@
 package agjoku.avancementsharing;
 
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -7,7 +8,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
+
 public class AdvancementSharing extends JavaPlugin implements Listener {
+    private final Set<String> advancementsTracked = new HashSet<>();
 
     @Override
     public void onEnable() {
@@ -16,18 +28,43 @@ public class AdvancementSharing extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
-        // プレイヤー名を取得
+    public void onPlayerAchievementAwarded(PlayerAdvancementDoneEvent event) {
+        String advancementName = event.getAdvancement().getKey().toString();
         String playerName = event.getPlayer().getName();
-        // 進捗名を取得
-        String advancementName = event.getAdvancement().getKey().getKey();
 
-        if(advancementName.startsWith("recipes/")) {
+        if(advancementsTracked.contains(advancementName) || advancementName.startsWith("minecraft:recipes/")) {
             return;
         }
 
+        advancementsTracked.add(advancementName);
+        writeAdvancementToCsv(playerName, advancementName);
+
         for (Player onlinePlayer : Bukkit.getOnlinePlayers()){
             onlinePlayer.sendMessage(playerName + "が進捗" + advancementName + "を達成しました");
+        }
+
+    }
+
+    private void writeAdvancementToCsv(String playerName, String advancementName) {
+        String csvFile = Paths.get(getDataFolder().getPath(), "advancements.csv").toString();
+        File file = new File(csvFile);
+        boolean isNewFile = !file.exists();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFile, true))) {
+            if (isNewFile) {
+                writer.write("PlayerName,AdvancementName,Date,Time"); // ヘッダー行を書く
+                writer.newLine();
+            }
+
+            LocalDateTime now = LocalDateTime.now();
+            String date = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+            writer.write(playerName + "," + advancementName + "," + date + "," + time);
+            writer.newLine();
+            getLogger().info("Recorded advancement: " + advancementName + " by " + playerName + " on " + date + " at " + time);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
